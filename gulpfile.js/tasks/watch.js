1e031c05c98ff2401ea8ed02d2a9dffb8f32b2c6
Dapen <spartdan@live.co.uk>
2019-08-20 14:22:48 +0100

    update lab

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -6,36 +6,23 @@ gulp.task("watch", function() {
   paths = {
     htmlSrc: [
       projectPath(PATH_CONFIG.BASE, PATH_CONFIG.html.src, "**/*.html"),
-      projectPath(PATH_CONFIG.lab, "**/*.html"),
-      projectPath(PATH_CONFIG.lab, "**/*.json")
+      projectPath(PATH_CONFIG.BASE, PATH_CONFIG.html.src, "**/*.json")
     ],
     stylesheetsSrc: [
-      projectPath(PATH_CONFIG.BASE, PATH_CONFIG.stylesheets.src, "**/*.scss"),
-      projectPath(PATH_CONFIG.lab, PATH_CONFIG.stylesheets.src, "**/*.scss")
+      projectPath(PATH_CONFIG.BASE, PATH_CONFIG.stylesheets.src, "**/*.scss")
     ],
-    javascriptsSrc: projectPath(
-      PATH_CONFIG.BASE,
-      PATH_CONFIG.javascripts.src,
-      "**/*.js"
-    ),
+    javascriptsSrc: [
+      projectPath(PATH_CONFIG.BASE, PATH_CONFIG.javascripts.src, "**/*.js")
+    ],
     imagesSrc: [
-      projectPath(
-        PATH_CONFIG.BASE,
-        PATH_CONFIG.images.src,
-        "**/*{jpg,png,svg}"
-      ),
-      projectPath(PATH_CONFIG.lab, PATH_CONFIG.images.src, "**/*{jpg,png,svg}")
+      projectPath(PATH_CONFIG.BASE, PATH_CONFIG.images.src, "**/*{jpg,png,svg}")
     ],
     iconsSrc: [projectPath(PATH_CONFIG.BASE, PATH_CONFIG.icons.src, "*.svg")]
   };
 
   gulp.watch(paths.htmlSrc, ["html", "lab:html", browser.reload]);
-  gulp.watch(paths.stylesheetsSrc, [
-    "stylesheets",
-    "lab:stylesheets",
-    browser.reload
-  ]);
+  gulp.watch(paths.stylesheetsSrc, ["stylesheets", browser.reload]);
   gulp.watch(paths.javascriptsSrc, ["webpack", browser.reload]);
-  gulp.watch(paths.imagesSrc, ["images, lab:images", browser.reload]);
+  gulp.watch(paths.imagesSrc, ["images", browser.reload]);
   gulp.watch(paths.iconsSrc, ["icons", browser.reload]);
 });
